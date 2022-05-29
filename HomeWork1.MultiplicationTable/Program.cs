3befa74a760bed43d08f0d8ab5878206660c587b
Denis <100792310+fidelden26@users.noreply.github.com>
2022-05-29 23:54:31 +0300

    Add project files.

---
+++ HomeWork1.MultiplicationTable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork1.MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Таблица умножения
+
+            Console.WriteLine("Multiplication table");
+            Console.WriteLine("enter a number");
+            var userInput = Console.ReadLine();
+            var number = 0;
+            var number2 = int.TryParse(userInput, out number);
+
+            Table(number);
+        }
+        static void Table(int number)
+        {
+            switch (number)
+            {
+                case < 11:
+
+                    for (int i = 0; i < 11; i++)
+                    {
+                        Console.WriteLine($"{number}*{i} = {number * i}");
+                    }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Only up to 10");
+                    break;
+            }
+        }
+    }
+}
---
+++ HomeWork1.Weekday/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace HomeWork1.Weekday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // неделя
+
+            Console.WriteLine("Weekday");
+            Console.WriteLine("Enter a number");
+            string userInpyt = Console.ReadLine();
+            var a = 0;
+            var userInput2 = int.TryParse(userInpyt, out a);
+
+            SevenDays(a);
+
+        }
+
+        static void SevenDays(int a)
+        {
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                default:
+                    Console.WriteLine("Only up to 7");
+                    break;
+
+            }
+        }
+    }
+}
